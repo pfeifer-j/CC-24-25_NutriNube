230b97070c0f4b9e5dd6d508b92973752188b4e4
Jan Pfeifer <pfeife@fim.uni-passau.de>
2024-11-14 18:41:04 +0100

    added configuration for fluentd and for marshmallow. Related to #10

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,19 +1,41 @@
 # app/__init__.py
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from fluent import asynchandler as fluentasynchandler
+from fluent import event
+import logging
 import os
+from flask_marshmallow import Marshmallow
 
 db = SQLAlchemy()
+ma = Marshmallow()
+
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     app.secret_key = os.urandom(24)
+    
+    # Setup Fluentd Logging
+    handler = fluentasynchandler.FluentHandler('app', host='localhost', port=24224)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.INFO)
 
+    # Add a simple formatter
+    format_string = '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    formatter = logging.Formatter(format_string)
+    handler.setFormatter(formatter)
+    
+    app.logger.info({
+        'event': 'fluentd_setup',
+        'message': 'Fluentd logger setup successfully!'
+    })
+    
     # Configure the database
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///default.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://pfeiferj:NutriNube@db:5432/flaskdb')
+    
     db.init_app(app)
 
     with app.app_context():
@@ -22,5 +44,14 @@ def create_app():
 
     from .routes import init_routes
     init_routes(app)
+
+    @app.before_request
+    def log_request_info():
+        app.logger.debug({ 
+            'event': 'request_received',
+            'method': request.method,
+            'url': request.url,
+            'headers': dict(request.headers)
+        })
 
     return app
--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,12 @@
 # app/app.py
 from app import create_app
+import logging
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    logging.info("Starting the Flask application")
+    try:
+        app.run(debug=True)
+    finally:
+        logging.info("Shutting down the Flask application")
