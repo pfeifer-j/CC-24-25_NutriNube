e6f066053a3c5da405b781318bc1bb29e6b9a0bb
Jan Pfeifer <pfeife@fim.uni-passau.de>
2025-01-10 16:37:04 +0100

    added a default port for deployment. Related to #17

--- src/app/app.py
+++ src/app/app.py
@@ -1,12 +1,14 @@
 # app/app.py
 from app import create_app
 import logging
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     logging.info("Starting the Flask application")
+    port = int(os.getenv('PORT', 5000))
     try:
         app.run(debug=True)
     finally:
